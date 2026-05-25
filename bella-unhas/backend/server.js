8a5fb59d2d7579136ade686cf52ffca210a0b3bc
rafael willian <rafarolins2017@gmail.com>
2026-05-25 01:18:36 -0400

    Update email transport configuration for nodemailer

--- bella-unhas/backend/server.js
+++ bella-unhas/backend/server.js
@@ -18,7 +18,9 @@ const pool = mysql.createPool({
 });
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
